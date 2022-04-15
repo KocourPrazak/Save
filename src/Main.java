dcc3b2fc40a320928b7ba37042a2300a279a6e7a
Andrei Aksenov <aksenovaa@hotmail.com>
2022-04-15 18:07:25 +0200

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -16,45 +16,46 @@ public class Main {
         GameProgress gameProgress2 = new GameProgress(90, 5, 9, 18.2);
         GameProgress gameProgress3 = new GameProgress(75, 2, 11, 32.8);
 
-        saveGame(gameProgress1, SAVEGAMES+"\\save1.dat");
-        saveGame(gameProgress2, SAVEGAMES+"\\save2.dat");
-        saveGame(gameProgress3, SAVEGAMES+"\\save3.dat");
+        saveGame(gameProgress1, SAVEGAMES + "\\save1.dat");
+        saveGame(gameProgress2, SAVEGAMES + "\\save2.dat");
+        saveGame(gameProgress3, SAVEGAMES + "\\save3.dat");
 
-        String[] fileList = {SAVEGAMES+"\\save1.dat", SAVEGAMES+"\\save3.dat"};
+        String[] fileList = {SAVEGAMES + "\\save1.dat", SAVEGAMES + "\\save3.dat"};
         zipFiles(fileList, SAVEGAMES + "\\savegame.zip");
 
-        for (String name: fileList) {
+        for (String name : fileList) {
             System.out.print("Delete " + name);
             File file = new File(name);
-            if (file.delete()){
+            if (file.delete()) {
                 System.out.println(" OK");
-            }else System.out.println(" Fail!");
+            } else System.out.println(" Fail!");
         }
 
-	// write your code here
+        // write your code here
     }
-    public static void saveGame(GameProgress gameProgress, String fileName){
+
+    public static void saveGame(GameProgress gameProgress, String fileName) {
 
         try (FileOutputStream fos = new FileOutputStream(fileName);
              ObjectOutputStream oos = new ObjectOutputStream(fos)) {
             System.out.println("Saving game progress to " + fileName);
             oos.writeObject(gameProgress);
-        }catch (Exception ex){
+        } catch (Exception ex) {
             System.out.println(ex.getLocalizedMessage());
         }
     }
 
-    public static void zipFiles(String[] fileList, String archiveName){
+    public static void zipFiles(String[] fileList, String archiveName) {
         System.out.println("Adding files to archive " + archiveName);
 
-        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(archiveName))){
+        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(archiveName))) {
             for (String name : fileList) {
 
                 int namePos = name.lastIndexOf("\\");
                 String sname = "";
 
-                if (namePos > 0){
-                    sname = name.substring(namePos+1);
+                if (namePos > 0) {
+                    sname = name.substring(namePos + 1);
                 }
 
                 System.out.println("adding " + name);
@@ -62,14 +63,15 @@ public class Main {
                 try (FileInputStream fis = new FileInputStream(name)) {
                     ZipEntry zen = new ZipEntry(sname);
                     zout.putNextEntry(zen);
-                    byte[] buffer = new byte[fis.available()];  fis.read(buffer);
+                    byte[] buffer = new byte[fis.available()];
+                    fis.read(buffer);
                     zout.write(buffer);
                     zout.closeEntry();
                 } catch (Exception ex) {
                     System.out.println(ex.getLocalizedMessage());
                 }
             }
-        }catch (Exception ex){
+        } catch (Exception ex) {
             System.out.println(ex.getLocalizedMessage());
         }
     }
